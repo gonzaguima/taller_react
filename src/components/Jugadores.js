6805a6cd3fada8a58350fef8b488dfeb0131e52a
gonzalo <gonzaguima@gmail.com>
2019-08-13 13:12:57 -0300

    Update Jugadores.js

--- src/components/Jugadores.js
+++ src/components/Jugadores.js
@@ -24,37 +24,37 @@ class Jugadores extends React.Component {
   render() {
     return (
       <div className="d-flex flex-column justify-content-center mt-5 pt-5">
-      <div className="m-auto w-50">
+      <div className="m-auto ">
         <div className='form-group'>
           <label htmlFor='playerName'>Nombre del jugador</label>
-          <input type='text' 
-          name='name' 
-          onChange={this.handleChange} 
-          className="form-control" 
+          <input type='text'
+          name='name'
+          onChange={this.handleChange}
+          className="form-control"
           placeholder='Ingrese nombre de su jugador' />
         </div>
         <div className='form-group'>
           <label htmlFor='playerLastName'>Apellido del jugador</label>
-          <input type='text' 
-          name='lastName' 
-          onChange={this.handleChange} 
-          className="form-control" 
+          <input type='text'
+          name='lastName'
+          onChange={this.handleChange}
+          className="form-control"
           placeholder='Ingrese apellido de su jugador' />
         </div>
         <div className='form-group'>
           <label htmlFor='playerBirth'>Fecha de nacimiento del jugador</label>
-          <input type='date' 
-          name='birthDate' 
-          onChange={this.handleChange} 
-          className="form-control" 
+          <input type='date'
+          name='birthDate'
+          onChange={this.handleChange}
+          className="form-control"
           placeholder='Ingrese fecha de nacimiento de su jugador' />
         </div>
         <div className='form-group'>
           <label htmlFor='number'>Numero del jugador</label>
-          <input type='text' 
-          name='number' 
-          onChange={this.handleChange} 
-          className="form-control" 
+          <input type='text'
+          name='number'
+          onChange={this.handleChange}
+          className="form-control"
           placeholder='Ingrese numero de su jugador' />
         </div>
         <input type='button' onClick={this.addPlayers} className="btn" value='Agregar equipo' />
